eddaeca1197c6839031399be1cc108cc5433055e
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-28 08:19:32 -0400

    implemented power

--- app.js
+++ app.js
@@ -20,4 +20,14 @@ app.get('/divide/:a/:b', (req, res) =>
   res.send((parseInt(req.params.a) / parseInt(req.params.b)).toString())
 )
 
+app.get('/power/:a/:b', (req, res) =>
+  res.send(
+    (
+      Math.pow(
+        parseInt(req.params.a),
+        parseInt(req.params.b)
+      ).toString()
+    ))
+)
+
 app.listen(3000)
