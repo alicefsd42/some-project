47ffa8832d6c9f3171701c3332040a2c89e6760d
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-28 08:04:42 -0400

    implemented add

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express()
 app.get('/hello', (req, res) =>
   res.send('Hello World'))
 
+app.get('/add/:a/:b', (req, res) =>
+  res.send((parseInt(req.params.a) + parseInt(req.params.b)).toString())
+)
+
 app.listen(4000)
