dfa36af0b38c00b4661ba32841b0b835c2390384
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-28 08:11:31 -0400

    implemented multiply

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ app.get('/subtract/:a/:b', (req, res) =>
   res.send((parseInt(req.params.a) - parseInt(req.params.b)).toString())
 )
 
+app.get('/multiply/:a/:b', (req, res) =>
+  res.send((parseInt(req.params.a) * parseInt(req.params.b)).toString())
+)
+
 app.listen(4000)
