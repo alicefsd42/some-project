0486c162b3383b6203b16d04688c3d4cac28a4c4
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-28 08:06:07 -0400

    implemented subtract

--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get('/add/:a/:b', (req, res) =>
   res.send((parseInt(req.params.a) + parseInt(req.params.b)).toString())
 )
 
+app.get('/subtract/:a/:b', (req, res) =>
+  res.send((parseInt(req.params.a) - parseInt(req.params.b)).toString())
+)
+
 app.listen(4000)
